44332c62582dbdc67bb7b317b773a8cb40034fc1
codesiness <codesiness@gmail.com>
2020-08-07 23:44:35 +0800

    f

--- resources/views/editor.blade.php
+++ resources/views/editor.blade.php
@@ -1,4 +1,3 @@
-@include('ckfinder::setup')
 <div class="{{$viewClass['form-group']}} {!! !$errors->has($errorKey) ? '' : 'has-error' !!}">
 
   <label for="{{$id}}" class="{{$viewClass['label']}} control-label">{{$label}}</label>
